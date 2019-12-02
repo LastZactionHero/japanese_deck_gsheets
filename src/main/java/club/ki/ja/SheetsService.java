78c6869b8e41246c3cae306e1a6ebd213cf530de
Zach <zdicklin@gmail.com>
2019-12-02 03:22:11 +0000

    Force approval prompt

--- src/main/java/club/ki/ja/SheetsService.java
+++ src/main/java/club/ki/ja/SheetsService.java
@@ -59,7 +59,7 @@ public class SheetsService {
     // Build flow and trigger user authorization request.
     GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(HTTP_TRANSPORT, JSON_FACTORY,
         clientSecrets, SCOPES).setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-            .setAccessType("offline").build();
+            .setAccessType("offline").setApprovalPrompt("force").build();
     LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
     return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
   }
